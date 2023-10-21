1363133b8727bebd47cf756cd770128b68cb5841
Avish Saini <82599778+avish-20@users.noreply.github.com>
2023-10-21 12:11:16 +0000

    datatypes

---
+++ 01_baics/01_variable.js
@@ -0,0 +1,12 @@
+const accountId = 144553
+let accountEmail = "avish@"
+var accountPassword = "12334"
+accountCity = "Jaipur"
+//accountId = 2; not allowed
+/*prefer not to use var
+because of issue of block*/
+let accountName;
+//js will consider it undefine
+console.table([accountId,accountEmail,accountPassword])
+
+
---
+++ 01_baics/02_dataTypes.js
@@ -0,0 +1,16 @@
+"use strict"; // treat all js code as newer version
+//alert() we are using nodejs not rowser
+let name = "avish"
+let age = 18
+let islogin = false
+//number
+///string
+//bool
+//undefined
+//null
+
+//Symbol
+
+//object
+console.log(typeof null);//object
+console.log(typeof undefine);//undefine
