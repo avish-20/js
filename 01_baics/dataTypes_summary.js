3c72923c2c80fc48cdcb54e98ae012c009227312
Avish Saini <82599778+avish-20@users.noreply.github.com>
2023-10-21 13:11:52 +0000

    dataType Summary

---
+++ 01_baics/dataTypes_summary.js
@@ -0,0 +1,20 @@
+"use strict"
+//primitive datatype
+// 7 types: string, number, null, bool, undefined, Symbo, BigInt
+//Refrance type(Non primitive)
+//Array, Objects, Functions
+//dynamically typed
+const id = Symbol("123")
+const id1 = Symbol("123")
+console.log(id === id1);// output is false
+const heros = ["shaktiman", "nagraj", "doga"]
+let myObject = {
+    name:"avish",
+    age:22
+}
+
+const myFun = function(){
+ 
+}
+
+
