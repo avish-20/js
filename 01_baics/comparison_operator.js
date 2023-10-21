9a2b6508dbcd3d00e1426be82935e229fb8e5b89
Avish Saini <82599778+avish-20@users.noreply.github.com>
2023-10-21 12:23:49 +0000

    operatorConversion

---
+++ 01_baics/comparison_operator.js
@@ -0,0 +1,20 @@
+"use strict"
+let score = "33"
+console.log(typeof score)
+console.log(typeof(score));
+let valueInNumber = Number(score)
+console.log(typeof valueInNumber)
+// "33" ->33
+//"33abc" -> Nan
+//true - 1
+let booleanIslogin  =1;
+let isLogin = Boolean(booleanIslogin)
+console.log(isLogin)
+
+// 1 - >true
+//"" ->false
+//"avish" - true
+let someNumber = 33;
+let stringNumber = String(someNumber)
+console.log(stringNumber);
+console.log(typeof stringNumber)
