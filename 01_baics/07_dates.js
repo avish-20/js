aa6e6c794c10dc31be64e41f414e6286763b451f
Avish Saini <82599778+avish-20@users.noreply.github.com>
2023-10-22 14:47:32 +0000

    Dates and time

---
+++ 01_baics/07_dates.js
@@ -0,0 +1,6 @@
+let myDate = new Date();
+console.log(myDate.toDateString());
+console.log(myDate.toLocaleDateString());
+console.log(typeof myDate);
+let myCreatedDate = new Date(2023, 0 , 23);
+console.log(myCreatedDate.toDateString());
